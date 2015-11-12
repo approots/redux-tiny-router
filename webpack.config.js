4d5e23fc701340a3a467ff41fb4d60fc715239b4
Rick Jolly <rickjolly@hotmail.com>
2015-11-12 00:44:06 -0800

    simplify webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,16 @@
 var webpack = require('webpack');
 var path = require('path');
 
-var reactExternal = {
-    root: 'React',
-    commonjs2: 'react',
-    commonjs: 'react',
-    amd: 'react'
-};
-
-
-var reduxExternal = {
-    root: 'redux',
-    commonjs2: 'redux',
-    commonjs: 'redux',
-    amd: 'redux'
-};
-
 module.exports = {
     //context: __dirname + "/src",
     entry: "./src/index.js",
     output: {
-        libraryTarget:'umd',
         path:  "./bin",
         filename: "reduxTinyRouter.js"
-
     },
     externals: {
-        'react': reactExternal,
-        'redux': reduxExternal
+        'react': 'React',
+        'redux': 'redux'
    //     'query-string':queryStringExternal
     },
   //  target:'node',
@@ -40,7 +23,7 @@ module.exports = {
                 test: /\.jsx?$/,
               //  loader:'babel?stage=0',
                loader:'babel?optional[]=runtime&stage=0',
-         //       loader:'babel?optional[]=runtime&stage=0',
+         //       loader:'babel?optional[]=runtime&stage=0&ignore=buffer',
                 include:[path.resolve('src')],
                 exclude: /node_modules/
             }
